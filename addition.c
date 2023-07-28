02e3c7517f133bd216c13b3755a525ab68e75993
VickyGhoderao <vickyghoderao0012@gmail.com>
2023-07-28 10:24:21 +0530

    2nd change

--- addition.c
+++ addition.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+void add();
 int main()
 {
+  add();
+}
+void add(){
   int a,b;
-  a=10;
-  b=20;
-  return a+b;
+  printf("Enter a two number:");
+  scanf("%d %d",&a,&b);
+  printf("addition of two number is:%d",a+b);
 }
