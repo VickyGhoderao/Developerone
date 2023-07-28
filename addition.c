02442f016035d6a418391ef8268c691482ff8e06
VickyGhoderao <vickyghoderao0012@gmail.com>
2023-07-28 09:55:32 +0530

    remove the funciton and insert into main function

--- addition.c
+++ addition.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-int add();
+
 int main()
 {
-  /*i have added varible tos*/
-  int a;
-  a=add();
-}
-int add(){
   int a,b;
-  printf("Enter a number including space:");
+  printf("Enter a number include space:");
   scanf("%d %d",&a,&b);
-  printf("Addition of two variable is:%d",a+b);
-  
-
+  printf("Addition of two number is:%d",a+b);
 }
